0dfb173961599462064711ef9467919fcae20626
jongrover <hello@jonathangrover.com>
2016-03-14 17:25:52 -0400

    size with capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,6 +7,12 @@ GreenOnion.configure do |c|
   }
 end
 
+RSpec.configure do |config|
+  config.before(:each) do
+    Capybara.page.driver.browser.manage.window.resize_to(1440, 768)
+  end
+end
+
 def create_screenshots
   GreenOnion.skin_visual('http://localhost:8000/index.html')
 end
@@ -19,10 +25,6 @@ def start_server_thread
     server.start
   end
   sleep 2
-end
-
-def exit_server_thread
-  @server_thread.exit
 end
 
 def exit_server_thread
